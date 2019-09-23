75549f1d62f502c98af2d4d16721ef9c5d1c2869
andrewhoff <ffohwerdna@gmail.com>
2019-09-22 17:31:27 -0700

    Allow specification of the openfaas address and function name in the route

--- handler.go
+++ handler.go
@@ -1,54 +1,71 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 
 	coap "github.com/go-ocf/go-coap"
 )
 
-type openFaasCallback string
+type openfaasCallback string
 
-type faasGetHandler struct {
-	cbName openFaasCallback
-	method coap.COAPCode
+type faasHandler struct {
+	OpenFaasFuncID openfaasCallback
+	Code           coap.COAPCode
+	Type           coap.COAPType
 }
 
-func (h faasGetHandler) ServeCOAP(w coap.ResponseWriter, r *coap.Request) {
+func (h faasHandler) ServeCOAP(w coap.ResponseWriter, r *coap.Request) {
 	var (
 		respBdy string
 		err     error
 	)
 
-	log.Printf("Got message: path=%q: %#v from %v\n\n", r.Msg.Path(), r.Msg, r.Client.RemoteAddr())
+	fmt.Println("sent", r.Msg.Code())
+	fmt.Println("expected", h.Code)
 
-	if r.Msg.Code() != h.method {
+	log.Printf("Got message: %#v path=%q: from %v\n", r.Msg.Path(), r.Msg, r.Client.RemoteAddr())
+
+	if r.Msg.Code() != h.Code {
 		w.SetCode(coap.MethodNotAllowed)
-		respBdy = "method not allowed"
+		respBdy = fmt.Sprintf("CoAP method: %q not allowed", r.Msg.Code())
 	} else {
 		// run openfaas function for route + verb
-		respBdy, err = openfaasCall(h.cbName, 2, 2)
+		respBdy, err = openfaasCall(h.OpenFaasFuncID, r.Msg.Payload())
 		if err != nil {
-			panic(err) // error invoking openfaas function
+			log.Printf("Error while trying to invoke openfaas function %v\n", err)
+			w.SetCode(coap.InternalServerError)
+			respBdy = fmt.Sprint("could not run callback for request")
 		}
 	}
 
-	ctx, cancel := context.WithTimeout(r.Ctx, time.Second)
+	ctx, cancel := context.WithTimeout(r.Ctx, 3*time.Second)
 	defer cancel()
 
 	log.Printf("Writing response to %v\n\n", r.Client.RemoteAddr())
-
 	w.SetContentFormat(coap.TextPlain)
 	if _, err := w.WriteWithContext(ctx, []byte(respBdy)); err != nil {
 		log.Printf("Cannot send response: %v", err)
 	}
 }
 
-func openfaasCall(name openFaasCallback, args ...interface{}) (string, error) {
-	// invoke openfaas function
-	// output := invokefaas(name, args)
-	output := "4"
-	return fmt.Sprintf("Invoked func: %v with args: %v. Result: %s", name, args, output), nil
+func openfaasCall(name openfaasCallback, bdy []byte) (string, error) {
+	resp, err := http.Post(fmt.Sprintf("%s/function/%s", openfaasAddress, string(name)), "application/octet-stream", bytes.NewBuffer(bdy))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	output := string(b)
+	return fmt.Sprintf("Invoked func: %v with input: %v. Result: %s", name, string(bdy), output), nil
 }
--- main.go
+++ main.go
@@ -8,38 +8,55 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-var port = flag.IntP("port", "p", 5683, "coap port to listen on")
+var (
+	openfaasAddress = "http://127.0.0.1:31112"
+	port            = flag.IntP("port", "p", 5683, "coap port to listen on")
+)
 
-// add flags to register routes + functions too (plumb through to openfaas)
+// add flags to register routes too (plumb through to openfaas)
 
-var methods = map[string]coap.COAPCode{
-	"GET":    coap.GET,
-	"POST":   coap.POST,
-	"PUT":    coap.PUT,
-	"DELETE": coap.DELETE,
-}
-
-type routeTuple struct {
-	Method   coap.COAPCode
-	Callback coap.Handler
-}
-
-// add registration ability for this in a bit
-var routes = map[string]routeTuple{
-	"/foo": routeTuple{
-		methods["GET"],
-		faasGetHandler{cbName: "foo.get", method: methods["GET"]},
-	},
+type routeTriple struct {
+	FuncName string
+	Verb     coap.COAPCode
+	Type     coap.COAPType
 }
 
 func main() {
+	flag.StringVarP(&openfaasAddress, "addr", "a", openfaasAddress, "openfaas gateway address")
 	flag.Parse()
 
 	mux := coap.NewServeMux()
-	for path, routeTuple := range routes {
-		mux.Handle(path, routeTuple.Callback)
+
+	registerRoutes(mux, routes)
+
+	fmt.Printf("serving CoAP requests on %d\n", *port)
+	log.Fatal(coap.ListenAndServe("udp", fmt.Sprintf(":%d", *port), mux))
+}
+
+var routes = map[string][]routeTriple{
+	"add": []routeTriple{
+		routeTriple{
+			Verb:     coap.POST,
+			Type:     coap.NonConfirmable,
+			FuncName: "add",
+		},
+	},
+	"go-fn": []routeTriple{
+		routeTriple{
+			Verb:     coap.GET,
+			Type:     coap.NonConfirmable,
+			FuncName: "go-fn",
+		},
+	},
+}
+
+func registerRoutes(mux *coap.ServeMux, routes map[string][]routeTriple) {
+	// this still does not support multiple verbs at the same route, will update
+	for k, rr := range routes {
+		for _, v := range rr {
+			mux.Handle(k, faasHandler{OpenFaasFuncID: openfaasCallback(v.FuncName), Code: v.Verb, Type: v.Type})
+		}
 	}
 
-	fmt.Printf("serving coap requests on %d\n", *port)
-	log.Fatal(coap.ListenAndServe("udp", fmt.Sprintf(":%d", *port), mux))
+	fmt.Printf("registering routes: %v\n", routes)
 }
